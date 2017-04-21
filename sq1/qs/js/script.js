eed6356ddd5e9eb0a6329e892f06b10d7097e462
jwkovell <jwkovell@gmail.com>
2017-04-21 13:48:52 -0400

    Delete script.js

--- sq1/qs/js/script.js
+++
@@ -1,9 +0,0 @@
-var Drupal = Drupal || { 'settings': {}, 'behaviors': {}, 'themes': {}, 'locale': {} };
-
-(function ($, Drupal) {
-  Drupal.behaviors.QsFrameworkLoad = {
-    attach: function (context, settings) {
-
-    }
-  };
-})(jQuery, Drupal);
